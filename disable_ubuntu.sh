c5d91f49232eff1eb5811988950882767cfc0bb8
niapoor <70398294+niapoor@users.noreply.github.com>
2024-03-07 11:26:02 -0500

    Create disable_ubuntu.sh

---
+++ disable_ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# == Author: Nia Poor ==
+# Short script to disable the ubuntu user
+
+# Lock the user
+sudo usermod -L ubuntu > /dev/null
+sudo passwd -l ubuntu > /dev/null
+# Expires the user
+sudo chage -E0 ubuntu > /dev/null
+# Change the user's shell to nologin
+sudo usermod -s /sbin/nologin ubuntu > /dev/null
+
+# Clear bash history
+sudo history -c
