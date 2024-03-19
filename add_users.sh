4fec1036ded941a37e66d0de8243787304bb5996
niapoor <70398294+niapoor@users.noreply.github.com>
2024-03-19 14:07:16 -0400

    created and (hopefully) completed add_users.sh

---
+++ add_users.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+# Author: Nia Poor
+# Description: A very hardcoded script to add a lot of junk users with very complex passwords to throw red team off in an in-class competition
+
+# Add a bunch of new users
+sudo useradd ansible2
+sudo useradd blue123
+sudo useradd red123
+sudo useradd test
+sudo useradd gray123
+sudo useradd red
+sudo useradd anotherone
+sudo useradd freeiphone
+sudo useradd blueORred
+sudo useradd redOld
+sudo useradd x_XALPHAX_x
+sudo useradd helloWorld
+sudo useradd spykid
+sudo useradd ssh_key
+sudo useradd blueTeam2
+sudo useradd gray
+sudo useradd melody
+sudo useradd mayhem
+sudo useradd nothx
+sudo useradd flappybird
+sudo useradd minecraftChicken
+sudo useradd NO
+sudo useradd stackpole
+sudo useradd AlexTA
+sudo useradd pikachu
+sudo useradd moneymaker
+sudo useradd jay
+sudo useradd nia
+sudo useradd zach
+sudo useradd jason
+sudo useradd csec473
+sudo useradd cdt
+sudo useradd alpha
+sudo useradd bravo
+sudo useradd charlie
+sudo useradd helper
+sudo useradd help
+sudo useradd 123456789O
+sudo useradd abcdefg
+sudo useradd green
+sudo useradd orange
+sudo useradd yellow
+sudo useradd purple
+sudo useradd yespls
+sudo useradd toomanyusers
+sudo useradd cyber_ranger
+sudo useradd range
+sudo useradd vuln
+sudo useradd alldone
+
+# Add passwords for the new users
+echo "ansible2":"hyudsrrvuydsrvuydgvygdvysr"| sudo chpasswd 1> /dev/null
+echo "blue123":"nvfisdbvfsdifvybdisvbillllll"| sudo chpasswd 1> /dev/null
+echo "red123":"h43bh87h88475h874bh84bh87b5h"| sudo chpasswd 1> /dev/null
+echo "gray123":"g832gh82824h83uigb93"| sudo chpasswd 1> /dev/null
+echo "test":"9gu3h98h8yhn98y5gh458ygh4"| sudo chpasswd 1> /dev/null
+echo "red":"85707248043275802375683475"| sudo chpasswd 1> /dev/null
+echo "anotherone":"758249453555555529054329"| sudo chpasswd 1> /dev/null
+echo "freeiphone":"43i51094857401175hgerw"| sudo chpasswd 1> /dev/null
+echo "blueORred":"g8534yhg59834hg8h84gh"| sudo chpasswd 1> /dev/null
+echo "redOld":"0i4j4g5jgggggggggggggggggggk09grj"| sudo chpasswd 1> /dev/null
+echo "x_XALPHAX_x":"hg54uhg54hgibewjfvbwrhvbuh"| sudo chpasswd 1> /dev/null
+echo "helloWorld":"9gh2hn9fy9yd2e2332323232322323333"| sudo chpasswd 1> /dev/null
+echo "spykid":"j0iwevjibnjfjrjfjfjjjjjjjjjf0293r"| sudo chpasswd 1> /dev/null
+echo "ssh_key":"0id2jdhrbubtygncurig493g439nb"| sudo chpasswd 1> /dev/null
+echo "blueTeam2":"pqfm48yvgb59ygfhcn329ycgb"| sudo chpasswd 1> /dev/null
+echo "gray":"p9q3cun94wf03ijf9fu3ng532ung9cm3h"| sudo chpasswd 1> /dev/null
+echo "melody":"87w4nr98yg76g2n4cr632n7gcrn3274cgr6n326cg"| sudo chpasswd 1> /dev/null
+echo "mayhem":"9503jg04j5g8j45g8j54gjg582j853gj834jg8j"| sudo chpasswd 1> /dev/null
+echo "nothx":"lovlwkvfkdfkvjfksvkfvkvfkjd"| sudo chpasswd 1> /dev/null
+echo "flappybird":"i3h87483f7heif403249jf03"| sudo chpasswd 1> /dev/null
+echo "minecraftChicken":"9h8hj98j949ggjdsnjdfsbvkdsfb"| sudo chpasswd 1> /dev/null
+echo "NO":"gjetuihiuehbiu8448848484848484884"| sudo chpasswd 1> /dev/null
+echo "stackpole":"0i0bjebtijjbgvhycrqwfdrct"| sudo chpasswd 1> /dev/null
+echo "AlexTA":"nvrkhbvjhrbrvjqkwwye865959836jf"| sudo chpasswd 1> /dev/null
+echo "pikachu":"94qu9qgj9j48848484888888884rfcdsa"| sudo chpasswd 1> /dev/null
+echo "moneymaker":"123456789008768900000000000032"| sudo chpasswd 1> /dev/null
+echo "csec473":"whyhiarvyviqbvredsucksnvbvisbvrs"| sudo chpasswd 1> /dev/null
+echo "cdt":"shrbgguygbvnuybgre85g6666666666666"| sudo chpasswd 1> /dev/null
+echo "alpha":"859w5ggggggggggggg5wgjuiew"| sudo chpasswd 1> /dev/null
+echo "jay":"password8fq3fb8vbyubvhdbvfj65444"| sudo chpasswd 1> /dev/null
+echo "bravo":"g8g387g248guurerrrrrrrrrrrrrrrrurruuuu"| sudo chpasswd 1> /dev/null
+echo "abcdefg":"eubwbhrtubudhbdskjbvrhbyvbeiw74ry"| sudo chpasswd 1> /dev/null
+echo "green":"849g9erg7yreg7ysecretmessage3i90423"| sudo chpasswd 1> /dev/null
+echo "nia":"g5428hg87gh8eyhuhbv"| sudo chpasswd 1> /dev/null
+echo "charlie":"382rjovhnvsgrbvuyygfvbsjku7"| sudo chpasswd 1> /dev/null
+echo "yellow":"20rj39jfviunhvewyrgf847gt"| sudo chpasswd 1> /dev/null
+echo "orange":"%%%%%bdwe%%%%%nfuiwh4v7bg4tv32ttgbv"| sudo chpasswd 1> /dev/null
+echo "purple":"!@#^@&^%743562874@!@#VHY"| sudo chpasswd 1> /dev/null
+echo "zach":"neou5hg7weggcqb3cy5cf77342c871yn"| sudo chpasswd 1> /dev/null
+echo "yespls":"938757328hcobgfywsyg35rvcu6rw"| sudo chpasswd 1> /dev/null
+echo "toomanyusers":"98765edcy5dvy5vfrejuewgheq8th"| sudo chpasswd 1> /dev/null
+echo "123456789O":"65t67vbt76t5bv736tvby3"| sudo chpasswd 1> /dev/null
+echo "help":"vhfdsjghdbugy38456329845cbbgwrvgb"| sudo chpasswd 1> /dev/null
+echo "jason":"10284762tfgcwiuefbvsju46536279"| sudo chpasswd 1> /dev/null
+echo "cyber_ranger":"95un8ynybiuewbtv4g7gbv2u34g"| sudo chpasswd 1> /dev/null
+echo "range":"8yn8b34hvbyegbtq3f4cu213vf2"| sudo chpasswd 1> /dev/null
+echo "vuln":"09u3rqnvybwievgtuewbvgteuy5vbgweytvgb"| sudo chpasswd 1> /dev/null
+echo "alldone":"94j9f834ncgbc364bgvf32i76vb"| sudo chpasswd 1> /dev/null
+echo "helper":"g73hg87wghvnwehvuewgyv"| sudo chpasswd 1> /dev/null
+
+# Clear bash history
+history -c
